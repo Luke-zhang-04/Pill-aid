2b56c8ad33f591ae8ac2b9ca50fd09fef04baae5
Matthew Zhou <myzhou@uwaterloo.ca>
2022-11-29 17:21:36 -0500

    read database achieved

--- services/client/src/pages/drugview/index.tsx
+++ services/client/src/pages/drugview/index.tsx
@@ -1,15 +1,35 @@
 import "~/index.scss"
 import {Form, Tile} from "carbon-components-react"
-import React from "react"
+import React, {useContext} from "react"
+import {collection, query, getDocs} from "firebase/firestore"
+import {AuthContext} from "~/contexts"
+import {db} from "~/firebase"
 
-export const Drugview: React.FC = () => (
-    <Form>
-        <div className="container">
-            <Tile className="content-container">
-                <h2>Drug Inventory</h2>
-            </Tile>
-        </div>
-    </Form>
-)
+export const Drugview: React.FC = () => {
+    const {currentUser} = useContext(AuthContext)
+
+    React.useEffect(() => {
+        ;(async () => {
+            if (currentUser) {
+                const que = query(collection(db, currentUser.uid))
+                const querySnapshot = await getDocs(que)
+
+                querySnapshot.forEach((doc) => {
+                    console.log(doc.data())
+                })
+            }
+        })()
+    }, [])
+
+    return (
+        <Form>
+            <div className="container">
+                <Tile className="content-container">
+                    <h2>Drug Inventory</h2>
+                </Tile>
+            </div>
+        </Form>
+    )
+}
 
 export default Drugview
