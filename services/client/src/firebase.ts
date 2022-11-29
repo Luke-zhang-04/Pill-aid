d131ec1d817cb2dfa0a4b3045a8681296181a611
Jacob Zhou <j39zhou@uwaterloo.ca>
2022-11-28 22:03:48 -0500

    form code from the guy on yt

--- services/client/src/firebase.ts
+++ services/client/src/firebase.ts
@@ -1,6 +1,8 @@
 import {initializeApp} from "firebase/app"
 import {GoogleAuthProvider, getAuth} from "firebase/auth"
 
+import {getFirestore, collection, getDocs} from "firebase/firestore"
+
 const firebaseConfig = {
     apiKey: "AIzaSyBi655f9iEAvVz7MIcnYOVvJqL6zLnEbrQ",
     authDomain: "pill-aid.firebaseapp.com",
@@ -9,6 +11,29 @@ const firebaseConfig = {
     messagingSenderId: "933725335363",
     appId: "1:933725335363:web:539e88cf2114be99b449f6",
 }
+
+// init firebase app
+initializeApp(firebaseConfig)
+
+// init services
+const db = getFirestore()
+
+// collection ref
+const colRef = collection(db, "form")
+
+// get collection data
+getDocs(colRef)
+    .then((snapshot) => {
+        const form = []
+
+        snapshot.docs.forEach((doc) => {
+            form.push({...doc.data(), id: doc.id})
+        })
+        console.log(form)
+    })
+    .catch((err) => {
+        console.log(err.message)
+    })
 
 export const app = initializeApp(firebaseConfig)
 
